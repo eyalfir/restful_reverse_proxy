3c184489d1d6c52f38cf6045491ccebd9f588e8d
Eyal Firstenberg <eyal.firstenberg@lightcyber.com>
2020-05-08 23:05:22 -0700

    added configuration in environment variable

--- app.py
+++ app.py
@@ -6,17 +6,19 @@ import sys
 import os
 import requests
 import copy
-from envsubst import envsubst
 import logging
 logging.basicConfig(stream=sys.stdout,
                     format='%(asctime)s | %(levelname)-8.8s | %(filename)s | %(process)d | %(message).10000s',
                     datefmt='%Y/%m/%d %H:%M:%S',
-                    level=logging.DEBUG if int(os.getenv('FLASK_DEBUG', '')) else INFO)
+                    level=logging.DEBUG if int(os.getenv('FLASK_DEBUG', '')) else logging.INFO)
 
 CORS_HEADER={'Access-Control-Allow-Origin': '*'}
 app = Flask(__name__)
-configuration_transformation = envsubst if os.getenv('ENVSUBST') else lambda x: x
-configuration = yaml.load(configuration_transformation(open(os.environ['CONFIG']).read()), Loader=yaml.FullLoader)
+if os.environ['CONFIG'].startswith('@'):
+    config = open(os.environ['CONFIG'].lstrip('@')).read()
+else:
+    config = os.environ['CONFIG']
+configuration = yaml.load(config, Loader=yaml.FullLoader)
 print("configuration is ")
 print(yaml.dump(configuration, sys.stdout))
 session = requests.session()
@@ -26,16 +28,13 @@ if 'CURL_CA_BUNDLE' in os.environ and not os.getenv('CURL_CA_BUNDLE'):
 def transform_object(obj, context):
     logging.debug('transforming %s of type %s', obj, type(obj))
     if isinstance(obj, str):
-        return var
-    elif 'jq' in obj:
+        return obj
+    if 'jq' in obj:
         script = obj['jq'] if isinstance(obj['jq'], str) else json.dumps(obj['jq'])
         logging.debug('jq script is %s of type %s', script, type(script))
         try:
             res = pyjq.first(script, context)
-            if isinstance(res, str):
-                return res
-            else:
-                return json.dumps(res)
+            return res if isinstance(res, str) else json.dumps(res)
         except pyjq._pyjq.ScriptRuntimeError as e:
             logging.error('cannot transform:')
             logging.error('object is %s', repr(obj['jq']))
@@ -47,16 +46,18 @@ def transform_object(obj, context):
 
 def get_response(upstream_config, context):
     config = upstream_config
-    if 'value' in config:
-        return Response(config['value'], status=200, content_type='application/json', headers=CORS_HEADER)
     v = {'env': json.dumps(dict(os.environ)), 'args': json.dumps(dict(request.args)), 'request': request.get_data().decode('utf-8')}
     url = transform_object(upstream_config['url'], context)
     method = upstream_config.get('method', 'get')
+    body = transform_object(upstream_config['body'], context) if 'body' in upstream_config else context['request']['body']
+    content_type = upstream_config.get('content_type', context['request']['content_type'])
     headers = {x: transform_object(y, context) for x, y in config.get('headers', {}).items()}
+    headers['Content-type'] = content_type
     logging.debug('url is %s', url)
     logging.debug('method is %s', method)
     logging.debug('headers is %s', headers)
-    req = requests.Request(method=method, url=url, headers=headers)
+    logging.debug('body is %s', str(body))
+    req = requests.Request(method=method, url=url, headers=headers, data=body)
     resp = session.send(req.prepare())
     logging.debug('got status %s from upstream', resp.status_code)
     return resp
@@ -65,7 +66,7 @@ def json_try(obj):
     try:
         return(json.loads(obj))
     except json.JSONDecodeError:
-        return null
+        return None
 
 for route in configuration:
     @app.route(route['path'], endpoint=route['path'], methods=route.get('methods', ['get']))
@@ -78,6 +79,7 @@ for route in configuration:
         context['request']['args'].update(kwargs)
         context['request']['body'] = request.get_data().decode('utf-8')
         context['request']['json'] = json_try(context['request']['body'])
+        context['request']['content_type'] = request.content_type
         context['request']['method'] = str(request.method)
         if 'upstream' in config:
             resp = get_response(config['upstream'], context)
@@ -92,18 +94,22 @@ for route in configuration:
             context['response'] = {
                     'body': body,
                     'json': json_try(body),
-                    'status_code': 200,
-                    'content_type': 'application/json'
+                    'status_code': int(config.get('status_code', 200)),
+                    'content_type': config.get('content_type', 'application/json')
             }
         if 'transformations' not in config:
             return Response(context['response']['body'], status=context['response']['status_code'], content_type=context['response']['content_type'], headers=CORS_HEADER)
         return transform(config['transformations'], context)
 
-    def transform(transformations, context):
-        code = context['response']['status_code']
-        matched_code = code if code in transformations else 'default'
-        logging.debug('matching transformation: %s', matched_code)
-        transformation = transformations[matched_code]
-        body = transform_object(transformation['body'], context) if 'body' in transformation else context['response']['body']
-        status = transform_object(transformation['status_code'], context) if 'status_code' in transformation else context['response']['status_code']
-        return Response(body, status=int(status), content_type='application/json', headers=CORS_HEADER)
+def transform_if_needed(transformation, prop, context):
+    return transform_object(transformation[prop], context) if prop in transformation else context['response'][prop]
+
+def transform(transformations, context):
+    code = context['response']['status_code']
+    matched_code = code if code in transformations else 'default'
+    logging.debug('matching transformation: %s', matched_code)
+    transformation = transformations[matched_code]
+    body = transform_if_needed(transformation, 'body', context)
+    status_code = transform_if_needed(transformation, 'status_code', context)
+    content_type = transform_if_needed(transformation, 'content_type', context)
+    return Response(body, status=int(status_code), content_type=content_type, headers=CORS_HEADER)
